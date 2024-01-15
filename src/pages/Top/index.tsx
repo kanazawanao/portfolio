8e9400be01818e52a6491f5401a68d3852548b51
kanazawanao <naomiti3333@gmail.com>
2024-01-15 23:50:41 +0900

    init

---
+++ src/pages/Top/index.tsx
@@ -0,0 +1,13 @@
+import Header from "components/Header";
+import Profile from "components/Profile";
+import Skill from "components/Skill";
+
+export default function TopPage() {
+  return (
+    <>
+      <Header />
+      <Profile />
+      <Skill />
+    </>
+  );
+}
