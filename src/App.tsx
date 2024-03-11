9183ba0ba7986266228a96e34b32bc565bef5016
kanazawanao <naomiti3333@gmail.com>
2024-03-11 14:02:17 +0900

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Profile from "components/Profile";
 import Skill from "components/Skill";
 
 export default function App() {
+  fetch("https://api.naz-pg.dev/ping")
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch(() => alert("error"));
   return (
     <>
       <Header />
